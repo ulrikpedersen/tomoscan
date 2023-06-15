8a5777060789fb0ad4182a8f3317a36ad0a27763
Bryn <31349775+8ryn@users.noreply.github.com>
2023-06-15 16:59:58 +0100

    Addition of sim pulse container and ophyd setup designed for docker compose

--- sim/pulsedLaser/simPulse.py
+++ sim/pulsedLaser/simPulse.py
@@ -1,3 +1,5 @@
+import time as ttime
+
 import cothread
 from softioc import builder, softioc
 
@@ -6,7 +8,7 @@ builder.SetDeviceName("laser")
 
 # Tune properties of laser (not runtime configurable)
 pulseLength = 0.01
-frequency = 10
+frequency = 1
 period = 1 / frequency
 assert period > pulseLength, "frequency is too high for pulse length"
 
@@ -16,6 +18,7 @@ laser_freq = builder.aIn(
     "freq", initial_value=frequency
 )  # This is simply for info, modifying the ai at runtime has no effect
 pulse_id = builder.aIn("pulse_id", initial_value=0)
+pulse_time = builder.aIn("pulse_time", initial_value=-1)
 
 # Start IOC
 builder.LoadDatabase()
@@ -28,6 +31,7 @@ def pulseLaser():
         cothread.Sleep(period - pulseLength)
         pulse_id.set(pulse_id.get() + 1)
         laser.set(1)
+        pulse_time.set(ttime.time())
         cothread.Sleep(pulseLength)
 
 
---
+++ src/tomoscan/ophyd_inter_setup.py
@@ -0,0 +1,109 @@
+# Designed to be used with iocs and simulators all running in docker compose
+
+import time as ttime
+
+from ophyd import ADComponent
+from ophyd import AreaDetector, SingleTrigger
+from ophyd import EpicsMotor
+from ophyd import Component, Device, EpicsSignal, EpicsSignalRO
+from ophyd import Kind
+from ophyd.areadetector import cam
+from ophyd.areadetector.filestore_mixins import FileStoreHDF5IterativeWrite
+from ophyd.areadetector.filestore_mixins import FileStoreTIFFIterativeWrite
+from ophyd.areadetector.plugins import HDF5Plugin_V34, TIFFPlugin_V34
+from bluesky import RunEngine
+from bluesky.plans import count, scan
+from bluesky.plan_stubs import mv
+import bluesky.plan_stubs as bps
+
+from bluesky.callbacks.best_effort import BestEffortCallback
+from databroker import Broker
+
+
+class MyHDF5Plugin(FileStoreHDF5IterativeWrite, HDF5Plugin_V34):
+    ...
+
+
+class MyTIFFPlugin(FileStoreTIFFIterativeWrite, TIFFPlugin_V34):
+    ...
+
+
+class MyDetector(SingleTrigger, AreaDetector):
+    cam = ADComponent(cam.AreaDetectorCam, "CAM:")
+    hdf1 = ADComponent(
+        MyHDF5Plugin,
+        "HDF1:",
+        write_path_template="/out/%Y/%m/%d",
+    )
+    tiff1 = ADComponent(
+        MyTIFFPlugin,
+        "TIFF1:",
+        write_path_template="/out/%Y/%m/%d",
+    )
+
+
+class MyLaser(Device):
+    power = Component(EpicsSignalRO, "power")
+    pulse_id = Component(EpicsSignalRO, "pulse_id")
+    pulse_time = Component(EpicsSignalRO, "pulse_time")
+    freq = Component(EpicsSignalRO, "freq", kind="config")
+
+
+# Heavily influenced by _wait_for_value function in epics_pvs.py, does block
+def wait_for_value(signal: EpicsSignal, value, poll_time=0.01, timeout=10):
+    expiration_time = ttime.time() + timeout
+    current_value = signal.get()
+    while current_value != value:
+        ttime.sleep(poll_time)
+        if ttime.time() > expiration_time:
+            raise TimeoutError(
+                "Timed out waiting for %r to take value %r after %r seconds"
+                % (signal, value, timeout)
+            )
+        current_value = signal.get()
+
+
+# Custom plan to move motor and then wait for laser pulse to take reading
+def pulse_sync(detectors, motor, laser, start, stop, steps):
+    step_size = (stop - start) / (steps - 1)
+
+    for det in detectors:
+        yield from bps.stage(det)
+
+    yield from bps.open_run()
+    for i in range(steps):
+        yield from bps.checkpoint()  # allows pausing/rewinding
+        yield from mv(motor, start + i * step_size)
+        wait_for_value(
+            laser.power, 0, poll_time=0.01, timeout=10
+        )  # Want to be at 0 initially such that image taken on pulse
+        wait_for_value(laser.power, 1, poll_time=0.001, timeout=10)
+        yield from bps.trigger_and_read(list(detectors) + [motor] + [laser])
+    yield from bps.close_run()
+
+    for det in detectors:
+        yield from bps.unstage(det)
+
+
+prefix = "ADT:USER1:"
+det = MyDetector(prefix, name="det")
+det.hdf1.create_directory.put(-5)
+det.hdf1.warmup()
+
+det.cam.stage_sigs["image_mode"] = "Single"
+det.cam.stage_sigs["acquire_time"] = 0.05
+
+motor1 = EpicsMotor("motorS:axis1", name="motor1")
+
+laser1 = MyLaser("laser:", name="laser1")
+laser1.wait_for_connection()
+
+RE = RunEngine()
+
+bec = BestEffortCallback()
+db = Broker.named("temp")  # This creates a temporary database
+
+# Send all metadata/data captured to the BestEffortCallback.
+RE.subscribe(bec)
+# Insert all metadata/data captured into db.
+RE.subscribe(db.insert)
