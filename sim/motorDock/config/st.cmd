d7e3710507a5d34c2b3bd5de6119afa28db6cf99
Bryn <31349775+8ryn@users.noreply.github.com>
2023-06-15 16:01:33 +0100

    Modify motor to network correctly in compose

--- sim/motorDock/config/st.cmd
+++ sim/motorDock/config/st.cmd
@@ -4,7 +4,7 @@ dbLoadDatabase "dbd/ioc.dbd"
 ioc_registerRecordDeviceDriver(pdbbase)
 
 #pmacAsynIPConfigure("geo1", "localhost:1025")
-pmacAsynIPConfigure("geo1", "0.0.0.0:1025")
+pmacAsynIPConfigure("geo1", "motor-sim:1025")
 pmacCreateController("motorS","geo1",0,8,50,500)
 pmacCreateAxis("motorS", 1)
 pmacCreateAxis("motorS", 2)
